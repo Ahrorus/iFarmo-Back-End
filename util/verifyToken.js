d74174dc6f743d120df27e4bda0555d25cbecf14
Ahror Abdulhamidov <ab.ahror@gmail.com>
2022-02-21 11:52:24 -0800

    Token verification added

--- util/verifyToken.js
+++ util/verifyToken.js
@@ -4,8 +4,8 @@ function verify(req, res, next) {
     const token = req.header('auth-token');
     if (!token) return res.status(401).send('Access Denied.');
     try {
-        const verifiedId = jwt.verify(token, process.env.TOKEN_SECRET);
-        req.user = verifiedId;
+        const verifiedUser = jwt.verify(token, process.env.TOKEN_SECRET);
+        req.user = verifiedUser;
         next();
     } catch (err) {
         res.status(400).send('Invalid token.');
