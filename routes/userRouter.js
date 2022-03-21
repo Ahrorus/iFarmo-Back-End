32ddb8e1493091c9d3df981891d6b743ce14d5ae
Ahror Abdulhamidov <ab.ahror@gmail.com>
2022-03-21 13:59:56 -0700

    Update userRouter.js

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -16,7 +16,7 @@ router.get('/', async (req, res) => {
 // Get a signle user by id
 router.get('/:userId', async (req, res) => {
     try {
-        const user = await User.findById(req.params.userId);
+        const user = await User.findById(req.params.userId).select('-password');
         res.json(user);
     } catch(err) {
         // If couldn't get the user
@@ -25,42 +25,42 @@ router.get('/:userId', async (req, res) => {
 });
 
 // Update user's info
-router.put('/:userId', async (req, res) => {
-    // Get auth-token from header
-    const token = req.header('auth-token');
-    if (!token) return res.status(401).send('Access Denied. Token required.');
-    // Verify the token
+router.put('/', async (req, res) => {
     try {
-        jwt.verify(token, process.env.TOKEN_SECRET);
+        // Get auth-token from header
+        const token = req.header('auth-token');
+        if (!token) return res.status(401).send('Access Denied. Token required.');
+        // Verify the token
+        try {
+            jwt.verify(token, process.env.TOKEN_SECRET);
+        } catch (err) {
+            res.status(400).send('Invalid token.');
+        }
+        const verifiedUser = jwt.verify(token, process.env.TOKEN_SECRET);
+        // Validate
+        const {error} = updateUserValidation(req.body);
+        if (error) {
+            return res.status(403).send(error.details[0].message);
+        }
+        // Try update the user
+        try {
+            const updatedUser = await User.findOneAndUpdate(
+                {_id: verifiedUser._id}, 
+                {$set: {
+                    name: req.body.name,
+                    role: req.body.role,
+                    bio: req.body.bio,
+                    contactInfo: req.body.contactInfo
+                }},
+                {new: true} // Makes sure to return the newly updatedUser
+            );
+            res.json(updatedUser);
+        } catch(err) {
+            // If couldn't update the user
+            res.status(404).send('Could not update the user.');
+        }
     } catch (err) {
-        res.status(400).send('Invalid token.');
-    }
-    const verifiedUser = jwt.verify(token, process.env.TOKEN_SECRET);
-    // If it's the wrong user
-    if (req.params.userId != verifiedUser._id) res.status(403).send('Unauthorized operation.');
-    // Validate
-    const {error} = updateUserValidation(req.body);
-    if (error) {
-        return res.status(403).send(error.details[0].message);
-    }
-    console.log(req.params.userId);
-    // Try update the user
-    try {
-        const updatedUser = await User.findOneAndUpdate(
-            {_id: req.params.userId}, 
-            {$set: {
-                name: req.body.name,
-                role: req.body.role,
-                bio: req.body.bio,
-                contactInfo: req.body.contactInfo
-            }},
-            {new: true} // Makes sure to return the newly updatedUser
-        );
-        console.log(updatedUser);
-        res.json(updatedUser);
-    } catch(err) {
-        // If couldn't update the user
-        res.status(404).send('Could not update the user.');
+        res.json({message: err});
     }
 });
 
